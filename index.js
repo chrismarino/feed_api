d833309a4bfd5ebefced53c2245f0d7c1c95d46b
Chris Marino <christopher.c.marino@gmail.com>
2023-12-19 09:16:15 -0800

    Live feed from local machine

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/hourly', (req, res) => res.render('pages/prices'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let app=express();
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/hourly', (req, res) => res.render('pages/prices'))
+  //res.setHeader('Content-Type', 'text/html')
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
